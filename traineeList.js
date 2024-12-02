9240c366718ee856ae9bd25af2b7dafa84082b1e
ACER <huyhoang432002@gmail.com>
2024-12-02 15:28:24 +0700

    247422-NguyenTanHuy

--- traineeList.js
+++ traineeList.js
@@ -1,5 +1,19 @@
 const JWAT_TRAINEES = {
     "203741": "Le Dang Hoang An",
     "247423": "Chung Quang Phuong"
-    "247420": "Ly Thanh Lam"
+    "247420": "Ly Thanh Lam",
+
+
+
+
+
+
+
+
+
+
+
+
+
+    '247422': 'Nguyen Tan Huy'
 }
