7ab550a343032d81278d57c40dfe736bf045d73c
longnh <longnh.uit@gmail.com>
2024-12-02 16:33:04 +0700

    test dev

---
+++ test2.js
@@ -0,0 +1,11 @@
+console.log("Hello World");
+
+console.log("Hello World2");
+
+console.log("This is in the test environment");
+
+    console.log("This is in the test environment");
+
+const sum = (a, b) => {
+    return a +          b;
+}
