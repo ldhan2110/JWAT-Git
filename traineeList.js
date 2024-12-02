12ee361fcc389933ff8203d0e844093c859d30a6
blackcat22521 <blackcat22521@gmail.com>
2024-12-02 15:27:19 +0700

    Add ten cua Loc

--- traineeList.js
+++ traineeList.js
@@ -19,6 +19,5 @@ const JWAT_TRAINEES = {
 
     '247422': 'Nguyen Tan Huy'
     "247423": "Chung Quang Phuong",
-    "247424": "Ngo Quang Nguyen",
-    "247420": "Ly Thanh Lam"
+    "247426": "Le Phuoc Gia Loc",
 }
