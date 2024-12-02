68cc310b37d3c65d63aa260fcf051c32b8f963eb
Le Dang Hoang An <an.le@cyberlogitec.com>
2024-12-02 16:21:05 +0700

    feat(config): add commitlint

--- 247419-NguyenHoangGiaHuy/index.js
+++ 247419-NguyenHoangGiaHuy/index.js
@@ -1,1 +1,3 @@
 console.log("This is pr Nguyen Hoang Gia Huy")
+
+console.log("Test husky format")
--- commitlint.config.js
+++ commitlint.config.js
@@ -26,7 +26,7 @@ module.exports = {
       'subject-empty': [2, 'never'],
       'subject-full-stop': [2, 'never', '.'],
       'header-max-length': [2, 'always', 100],
-      'footer-empty': [2, 'never'],
+      'footer-empty': [2, 'always'],
     },
   };
   
