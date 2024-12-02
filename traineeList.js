41749a337a4ba25ca491240b99dcc6ed5b924e6f
Ari <bichngoclenguyen1204@gmail.com>
2024-12-02 15:44:31 +0700

    Bich Ngoc commit

--- traineeList.js
+++ traineeList.js
@@ -10,5 +10,6 @@ const JWAT_TRAINEES = {
     "247423": "Chung Quang Phuong",
     "247426": "Le Phuoc Gia Loc",
     "247429": "Ly Nguyen An Khang",
-    "247418": "Nguyen Hoang long"
+    "247418": "Nguyen Hoang long",
+    "247421": "Le Nguyen Bich Ngoc"
 }
