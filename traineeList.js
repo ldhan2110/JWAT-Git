d9d6b53c3d6e550c4cb7a54f77ada09ab01bbdb3
ngoquangnguyen111 <ngoquangnguyen111@gmail.com>
2024-12-02 15:47:33 +0700

    add Nguyen

--- traineeList.js
+++ traineeList.js
@@ -2,14 +2,15 @@ const JWAT_TRAINEES = {
     "203741": "Le Dang Hoang An",
     "247428": "Sa Dam",
     "247419": "Nguyen Hoang Gia Huy",
-    "247423": "Chung Quang Phuong"
+    "247423": "Chung Quang Phuong",
     "247427": "Phan The Minh",
     "247420": "Ly Thanh Lam",
     "247425": "Le Ta Hoang Chuong",
-    '247422': 'Nguyen Tan Huy'
+    '247422': 'Nguyen Tan Huy',
     "247423": "Chung Quang Phuong",
     "247426": "Le Phuoc Gia Loc",
     "247429": "Ly Nguyen An Khang",
     "247418": "Nguyen Hoang long",
-    "247421": "Le Nguyen Bich Ngoc"
+    "247421": "Le Nguyen Bich Ngoc",
+    "247424": "Ngo Quang Nguyen",
 }
