88ae3d41a3a702a8d9f3169602faa879bff9a306
Lê Tạ Hoàng Chương <hoangchuongleta@gmail.com>
2024-12-02 15:30:32 +0700

    add name

--- traineeList.js
+++ traineeList.js
@@ -1,8 +1,8 @@
 const JWAT_TRAINEES = {
     "203741": "Le Dang Hoang An",
-    "247423": "Chung Quang Phuong"
+    "247423": "Chung Quang Phuong",
     "247420": "Ly Thanh Lam",
-
+    "247425": "Le Ta Hoang Chuong",
 
 
 
